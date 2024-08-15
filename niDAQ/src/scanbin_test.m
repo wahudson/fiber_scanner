154d3f7e2ef059b7e70418ad1224d45846d5693e
William Hudson <wahudson>
2024-08-14 21:58:36 -0600

    niDAQ:  scanbin_test.m  Bug fixes, output format fixes.
        Input for-loop over all of inVec.
        Output index kk increment only on output.
        Reshape linVec for image display.

--- niDAQ/src/scanbin_test.m
+++ niDAQ/src/scanbin_test.m
@@ -12,8 +12,10 @@
     SampleX_n     =   1250;		% sinusoidal input
     SampleY_n     =   400;
 
-    LinX_n        =   400;		% Linearized output
+    LinX_n        =   400;		% Linearized output, X FOV
     LinY_n        =   SampleY_n;
+
+    Lin2X_n       =   2 * LinX_n;	% full cycle
 
 	% Sweep origin is upper-left corner (-1.0, -1.0).  Galvo axis are:
 	%   +X axis is right, dx > 0 positive
@@ -28,6 +30,7 @@
     fprintf( 'SampleY_n     = %10d\n',   SampleY_n     );
     fprintf( 'LinX_n        = %10d\n',   LinX_n        );
     fprintf( 'LinY_n        = %10d\n',   LinY_n        );
+    fprintf( 'Lin2X_n       = %10d\n',   Lin2X_n       );
 
 %% Load data
     daq_file = "out16-daq-x1250.dat";
@@ -45,27 +48,35 @@
 
     linVec = scanbin( sigVec, SampleX_n, LinX_n );
 
-%% Save result data
-
     linVec_n = length( linVec );
     fprintf( 'linVec_n      = %10d\n',   linVec_n      );
 
-    linVec_file = OfileBase + "-lin-x" + LinX_n + ".dat";
+    rasterIm = transpose( reshape( linVec, Lin2X_n, linY_n ) );
+	% Raw raster matrix, upright image, mirrored X.
+	% Function reshape( .., Nrow, Ncol ) generates output array by column,
+	% leaving the image transposed.
+
+    rasterIu = rasterIm( :, [1:LinX_n] );
+	% Single FOV scaning left to right.
+
+%% Save result data
+
+    linVec_file = OfileBase + "-lin2-x" + Lin2X_n + ".dat";
     file_id = fopen( linVec_file, 'w' );
-    fprintf( file_id, '%8.5f\n', transpose( linVec ) );
+    fprintf( file_id, '%8.5f\n', linVec );
 	% Vectors applied in column order.
     fclose( file_id );
-    fprintf( 'linVec_file    = %s\n', linVec_file );
+    fprintf( 'linVec_file   = %s\n', linVec_file );
 
 %% Show image
 
     fig1 = figure(1);  clf;
-    imshow( linVec, DisplayRange=[sigMin_V, sigMax_V] );
+    imshow( rasterIm, DisplayRange=[sigMin_V, sigMax_V] );
 	% Display each row horizontally, i.e. matrix is right-side-up.
 
     fig1_file = OfileBase + "-fig1.jpg";
     exportgraphics( fig1, fig1_file );
-    fprintf( 'fig1_file      = %s\n', fig1_file );
+    fprintf( 'fig1_file     = %s\n', fig1_file );
 
 %%--------------------------------------------------------------------------
 
@@ -73,21 +84,26 @@ function [ outVec ] = scanbin( inVec, Nxi, Nxb )  % {
     %
     % outVec =  output data column vector, cycle (2*Nxb)
     % inVec  =  input  data column vector, cycle Nxi
-    % Nxi    =  number of samples in one cycle of Xi
-    % Nxb    =  number of output bins across X FOV, Nxb << Nxi
+    % Nxi    =  number of samples in one cycle of Xi (i.e. 2*FOV)
+    % Nxb    =  number of output bins across X FOV, Nxb << Nxi/2
 
     Nxi2 = int32( Nxi / 2 );			% round toward zero??
     bx2  = double( 1.0 ) / double( Nxb );	% half width of a bin
 
+    inVec_n = length( inVec );
+    outN_n  = (inVec_n * (2 * Nxb)) / Nxi;	% estimate output length
+
+    fprintf( '   scanbin()\n' );
     fprintf( 'Nxi           = %10d\n',   Nxi           );
     fprintf( 'Nxb           = %10d\n',   Nxb           );
     fprintf( 'Nxi2          = %10d\n',   Nxi2          );
     fprintf( 'bx2           = %10.6f\n', bx2           );
+    fprintf( 'outN_n        = %10d\n',   outN_n        );
 
     %% Compute Xmap[] array
 
     Xmap   = int32(  zeros(    Nxi,  1 ) );	% map column vector
-    outVec = double( zeros( (2*Nxb), 1 ) );	% output column vector
+    outVec = double( zeros( outN_n,  1 ) );	% output column vector
 
     % Initial values ensure nn=0 is skipped in the map.
     nn  = 0;			% output bin index
@@ -114,7 +130,7 @@ function [ outVec ] = scanbin( inVec, Nxi, Nxb )  % {
 
     for  ii = [0:Nxi2]		% half cycle
 
-        Xmap(Nxi2 + ii) = Xmap(Nxi2 - ii);
+        Xmap(Nxi2 + ii + 1) = Xmap(Nxi2 - ii + 1);
     end
 
     %% Linearize signal vector:
@@ -125,13 +141,13 @@ function [ outVec ] = scanbin( inVec, Nxi, Nxb )  % {
     pcnt  = 0;		% current bin count of samples accumulated
     kk    = 0;		% output outVec index
 
-    for  ix = [1:Nxi]		% { each signal value index
+    for  ix = [1:inVec_n]	% { each signal value index
 
-	kk = kk + 1;
 	ni = Xmap(jj+1);
 
-	if ( ni != np )
+	if ( ni ~= np )		% not equal !=
 
+	    kk = kk + 1;
 	    outVec(kk) = double( pVsum ) / double( pcnt );
 
 	    pVsum = double( 0.0 );
